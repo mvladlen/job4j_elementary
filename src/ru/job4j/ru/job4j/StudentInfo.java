2e23997ed7da76eb9270330d00e365fa6f1d253c
mvladlen <vladlen@ufanet.ru>
2020-11-22 21:07:58 +0500

    3.3. Разбор первой программы. [#414283]

---
+++ src/ru/job4j/ru/job4j/StudentInfo.java
@@ -0,0 +1,11 @@
+package ru.job4j.ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Vladlen Mikhailin");
+        System.out.println("01/09/1979");
+
+
+
+    }
+}
