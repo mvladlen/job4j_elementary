f598e036c089e61624fa2c47e07013656e657ca6
mvladlen <vladlen@ufanet.ru>
2020-11-27 20:18:12 +0500

    1. Чтение кода и базовые ошибки [#414402]

--- src/ru/job4j/array/ArrayChar.java
+++ src/ru/job4j/array/ArrayChar.java
@@ -2,8 +2,8 @@ package ru.job4j.array;
 
 public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
-        for (int i = 0; i < pref.length; i++) {
-            if (word[i] != pref[i]) {
+        for (int index = 0; index < pref.length; index++) {
+            if (word[index] != pref[index]) {
                 return false;
             }
         }
