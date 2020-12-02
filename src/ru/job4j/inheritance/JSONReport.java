4b5ea71ae531d01943a3124377b551fa341ac63f
mvladlen <vladlen@ufanet.ru>
2020-12-02 08:41:35 +0500

    4. Переопределение [#414436] убрана запятая

--- src/ru/job4j/inheritance/JSONReport.java
+++ src/ru/job4j/inheritance/JSONReport.java
@@ -6,7 +6,7 @@ public class JSONReport extends TextReport {
     public String generate(String name, String body) {
         return "{" + System.lineSeparator()
                 + " \"name\" : \"" + name + "\"," + System.lineSeparator()
-                + " \"body\" : \"" + body + "\"," + System.lineSeparator()
+                + " \"body\" : \"" + body + "\"" + System.lineSeparator()
                 + "}";
     }
 }
