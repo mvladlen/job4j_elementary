03283ba279db1fb5b9cb8a5ff7c43eb53996a1a8
mvladlen <vladlen@ufanet.ru>
2020-12-04 12:22:06 +0500

    3. Массивы и модели. [#414444]

--- src/ru/job4j/pojo/Library.java
+++ src/ru/job4j/pojo/Library.java
@@ -11,17 +11,17 @@ public class Library {
         array[1] = spaceFiction;
         array[2] = popular;
         array[3] = vocabulary;
-        for (int index = 0; index < 4; index++) {
+        for (int index = 0; index < array.length; index++) {
             System.out.println(array[index].getName());
         }
         Book temp;
         temp = array[0];
         array[0] = array[3];
         array[3] = temp;
-        for (int index = 0; index < 4; index++) {
+        for (int index = 0; index < args.length; index++) {
             System.out.println(array[index].getName());
         }
-        for (int index = 0; index < 4; index++) {
+        for (int index = 0; index < array.length; index++) {
             Book book = array[index];
             if (book.getName().equals("Clean Code")) {
                 System.out.println(array[index].getName());
