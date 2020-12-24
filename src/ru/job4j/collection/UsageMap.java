cfad154cc2ff21e4fe4d29771b31fd359bdffe35
mvladlen <vladlen@ufanet.ru>
2020-12-24 07:46:28 +0500

    0. Map, HashMap [#414359]

---
+++ src/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,17 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("parsentev@yandex.ru", "Petr Arsentev");
+        map.put("parsentev@yandex.ru", "Petr Arsentev");
+        map.put("iarsentev@yandex.ru", "Ivan Arsentev Sergeevich");
+
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}
