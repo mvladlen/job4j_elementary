13ad84b85c24d1b441597543d647904a88083bfd
mvladlen <vladlen@ufanet.ru>
2021-01-02 17:54:35 +0500

    2. Комбинированный компаратор [#414348]

--- src/ru/job4j/collection/User.java
+++ src/ru/job4j/collection/User.java
@@ -3,8 +3,8 @@ package ru.job4j.collection;
 import java.util.Objects;
 
 public class User implements Comparable<User> {
-    private String name;
-    private int age;
+    private final String name;
+    private final int age;
 
     public User(String name, int age) {
         this.name = name;
@@ -13,23 +13,35 @@ public class User implements Comparable<User> {
 
     @Override
     public int compareTo(User o) {
-        if (this.name.equals(o.name))
+        if (this.name.equals(o.name)) {
             return Integer.compare(this.age, o.age);
-
+        }
         return this.name.compareTo(o.name);
     }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         User user = (User) o;
         return age == user.age
-                &&  Objects.equals(name, user.name);
+                && Objects.equals(name, user.name);
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(name, age);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
     }
 }
